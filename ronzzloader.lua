2e609b8184dd3d0d1b4010eba5cc1416b197a4a5
ronzzofficial <ronzz191002@gmail.com>
2026-07-29 15:05:47 +0800

    Update and rename loader6.lua to ronzzloader.lua

--- ronzzloader.lua
+++ ronzzloader.lua
@@ -28,7 +28,7 @@ local LocalPlayer =
 --==================================================
 
 local MAIN_URL =
-    "https://raw.githubusercontent.com/ronzzofficial/gag1/refs/heads/main/test6.lua?v="
+    "https://raw.githubusercontent.com/ronzzofficial/gag1/refs/heads/main/ronzzholy.lua?v="
     .. tostring(os.time())
 
 --==================================================
